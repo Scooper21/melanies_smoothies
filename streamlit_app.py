bac82dac106afbef834d73b4787424e76be448a1
Scooper21 <33223727+Scooper21@users.noreply.github.com>
2024-03-14 12:35:35 -0400

    Update streamlit_app.py
    Dynamic nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -29,6 +30,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list: 
         ingredients_string+= fruit_chosen + ' '
+        st.subheader(fruit_chosen+ ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width = True)
 
     st.write(ingredients_string)
 
@@ -44,7 +48,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width = True)
+
+
